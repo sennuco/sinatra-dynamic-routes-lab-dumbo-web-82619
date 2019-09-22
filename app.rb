bd52de6125cea149a6e7ae79823dd4ff69290afe
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-22 23:01:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,8 +30,8 @@ class App < Sinatra::Base
    
    get '/:operation/:number1/:number2' do
      @operation = params[:operation]
-     @number1 = params[:number1]
-     @number2 = params[:number2]
+     @number1 = params[:number1].to_i
+     @number2 = params[:number2].to_i
      
      case @operation
      when 'add'
