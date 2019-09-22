4674a0a0bff7ed2d0d8f3201688c7937e4481b2c
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-22 22:54:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -29,6 +29,13 @@ class App < Sinatra::Base
    end
    
    get '/:operation/:number1/:number2' do
+     @operation = params[:operation]
+     @number1 = params[:number1]
+     @number2 = params[:number2]
+     
+     case @operation
+     when 'add'
+       
      
    end
    
