b9b84d689e00f75d9335ab5b65041584196b84a5
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-22 22:39:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,10 @@ class App < Sinatra::Base
  end
    
    get "/say/:number/:phrase" do
+     @number = params[:number].to_i
+     @phrase = params[:phrase]
      
+     "#{@phrase * @number}"
      
    end
 
